f9cb144f3b3779be2f9469c794de2f5e7182d735
geraldjgsupit052005-lab <geraldjgsupit052005@gmail.com>
2026-03-27 03:04:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function togglePassword(){
+var pass = document.getElementById("password");
+pass.type = pass.type === "password" ? "text" : "password";
+}
+
+function toggleDark(){
+document.body.classList.toggle("dark");
+}
+
+function toggleMenu(){
+var menu = document.getElementById("navMenu");
+menu.classList.toggle("show");
+}
+
+function login(e){
+e.preventDefault();
+
+document.getElementById("landing").style.display="none";
+document.querySelector(".partners").style.display="none";
+document.querySelector(".modules").style.display="none";
+
+document.getElementById("dashboard").style.display="block";
+}
